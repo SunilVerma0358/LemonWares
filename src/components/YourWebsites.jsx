6eec7908e5f575b2b454f2da8303dba70bdc50a3
SunilVerma0358 <sunilverma93060@gmail.com>
2024-01-26 10:37:07 +0530

    text Size

--- src/components/YourWebsites.jsx
+++ src/components/YourWebsites.jsx
@@ -17,7 +17,7 @@ const YourWebsites = () => {
           <div className=" sm:max-w-[72px] max-w-[37px] absolute right-[-1%] bottom-[9%]">
             <img className="w-full" src={ThreeElipse} alt="img" />
           </div>
-          <h2 className=" font-Poppins font-semibold text-[#2E2E2E] text-[38px] sm:text-[48px] md:text-[56px] lg:text-[64px] leading-[137%] max-w-[1024px] mx-auto text-center">
+          <h2 className=" font-Poppins font-semibold text-[#2E2E2E] max-[360px]:text-[28px] text-[30px] sm:text-[48px] md:text-[56px] lg:text-[64px] leading-[120%] sm:leading-[137%] max-w-[1024px] mx-auto text-center">
             We're here to make your website awesome.
           </h2>
           <button className="bg-[#B00000] rounded-[8px]  font-Poppins font-normal text-[#fff] text-[12px] sm:text-[14px] leading-[171%] flex items-center gap-[9px] px-4 py-3 mx-auto mt-[22px] sm:mt-[40px] hover:shadow-[0px_0px_64px_0px_#b000004d] transition-all duration-300 ease-linear ">
